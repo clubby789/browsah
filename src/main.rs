c8a89063f7531623a41167136d7e308b591fe6cf
clubby789 <jamie@hill-daniel.c.uk>
2021-12-25 00:16:55 +0000

    Add some tests

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,37 @@
 extern crate clap;
 extern crate nom;
-extern crate reqwest;
-
 use clap::Parser;
+
+use std::fs;
 
 mod css;
 mod html;
 
 #[derive(Parser, Debug)]
 struct Args {
-    #[clap(short, long)]
+    #[clap(short, long, help = "CSS File")]
     pub css: Option<String>,
 
-    #[clap(short, long)]
+    #[clap(short, long, help = "HTML File")]
     pub html: Option<String>,
 }
 
 fn main() {
-    println!("Hello, world");
+    let args = Args::parse();
+    if let Some(file) = args.html {
+        let data = fs::read_to_string(file).unwrap();
+        let (remaining, parsed) = html::parse_dom_node(data.as_str()).expect("Could not parse HTML");
+        println!("Parsed: {:#?}", parsed);
+        if remaining.len() > 0 {
+            eprintln!("Could not parse: {}", remaining);
+        }
+    }
+    if let Some(file) = args.css {
+        let data = fs::read_to_string(file).unwrap();
+        let (remaining, parsed) = css::stylesheet(data.as_str()).expect("Could not parse CSS");
+        println!("Parsed: {:#?}", parsed);
+        if remaining.len() > 0 {
+            eprintln!("Could not parse: {}", remaining);
+        }
+    }
 }
